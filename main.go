f08851bc27062b3a7dff31affc2eebca1162992f
Grapeka <chotwilaiwan@gmail.com>
2023-01-07 20:25:38 +0700

    `feat` : connect db, add init functions

--- main.go
+++ main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
+	"log"
 	"os"
-	"todo/initializers"
+	"todo/db"
+	"todo/routers"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
+
+func InitRoutes(router *gin.Engine) {
+	routers.SetTodoRoutes(router)
+}
+
+func LoadEnvVariables() {
+	err := godotenv.Load()
+	if err != nil {
+	  log.Fatal("Error loading .env file")
+	}
+}
 
 func main() {
 	router := gin.Default()
 
-	initializers.LoadEnvVariables()
-	initializers.InitRoutes(router) 
+	LoadEnvVariables()
+	InitRoutes(router)
+	db.ConnectToDataBase()
+	db.SyncDB()
 
 	port := os.Getenv("PORT")
-	
 	router.Run("localhost:" + port)
 }
