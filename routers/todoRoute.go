afc8363d03f26385840e7eec471b461cab562af9
Grapeka <chotwilaiwan@gmail.com>
2023-01-09 13:51:22 +0700

    `feat` : add PUT method for /todo/:id endpoint

--- routers/todoRoute.go
+++ routers/todoRoute.go
@@ -10,5 +10,6 @@ func SetTodoRoutes(router *gin.Engine) {
 	router.GET("/todos", controllers.GetTodos)
 	router.GET("/todo/:id", controllers.GetTodo)
 	router.POST("/todo", controllers.AddTodo)
+	router.PUT("/todo/:id", controllers.UpdateTodo)
 	router.DELETE("/todo/:id", controllers.DeleteTodo)
 }
